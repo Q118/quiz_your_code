dba9c051a9dd6e984fb90c2d272fc469ef080f24
Q118 <shelbyfish91@gmail.com>
2020-06-17 16:39:00 -0700

    functional answers for first question

--- Assets/alternate.js
+++ Assets/alternate.js
@@ -28,117 +28,143 @@ var dContainer = document.getElementById("dChoice");
 
 //set up array to display all the Q/As, using object properties
 var questions = [
-    {
-        question: "Which of the following is not a semantic element?",
-        answers: {
-            a: "section",
-            b: "article",
-            c: "div",
-            d: "heder",
-        },
-        correctAnswer: "c",
-    },
-    {
-        question:
-            "Media queries define how css styles are applied in relation to the characteristics of the _________",
-        answers: {
-            a: "window",
-            b: "console",
-            c: "device viewport",
-            d: "server",
-        },
-        correctAnswer: "c",
-    },
-    {
-        question: "In CSS, when/where must media queries be displayed? ",
-        answers: {
-            a: "first",
-            b: "anywhere",
-            c: "in a seperate file",
-            d: "last",
-        },
-        correctAnswer: "d",
-    },
-    {
-        question: "Who invented Javascript?",
-        answers: {
-            a: "Elon Musk",
-            b: "Brendan Eich",
-            c: "Bill Gates",
-            d: "Steve Jobs",
-        },
-        correctAnswer: "b",
-    },
+	{
+		question: "Which of the following is not a semantic element?",
+		answers: {
+			a: "section",
+			b: "article",
+			c: "div",
+			d: "heder",
+		},
+		correctAnswer: "c",
+	},
+	{
+		question:
+			"Media queries define how css styles are applied in relation to the characteristics of the _________",
+		answers: {
+			a: "window",
+			b: "console",
+			c: "device viewport",
+			d: "server",
+		},
+		correctAnswer: "c",
+	},
+	{
+		question: "In CSS, when/where must media queries be displayed? ",
+		answers: {
+			a: "first",
+			b: "anywhere",
+			c: "in a seperate file",
+			d: "last",
+		},
+		correctAnswer: "d",
+	},
+	{
+		question: "Who invented Javascript?",
+		answers: {
+			a: "Elon Musk",
+			b: "Brendan Eich",
+			c: "Bill Gates",
+			d: "Steve Jobs",
+		},
+		correctAnswer: "b",
+	},
 ];
 
 instructionsContainer.textContent =
 	"Welcome to the quiz on coding! Once you press start,you will have 75 seconds to complete this multiple choice quiz. If you get a question wrong, time will be deducted. You're final score will be the number of seconds left. Post your initials with your score at the end!";
 
-    //provide radio buttons for answers
-    //answers.push(
-        //`<label>
-         // <input type="radio" name="question${questionNumber}" value="${letter}">
-          //${letter} :
-       //   ${currentQuestion.answers[letter]}
-     //   </label>`
-   // );
+//provide radio buttons for answers
+//answers.push(
+//`<label>
+// <input type="radio" name="question${questionNumber}" value="${letter}">
+//${letter} :
+//   ${currentQuestion.answers[letter]}
+//   </label>`
+// );
 
 //this hides the quiz elements when page first loads
-	window.addEventListener("load", function (event) {
-		submitButton.style.visibility = "hidden";
-		nextButton.style.visibility = "hidden";
-        backButton.style.visibility = "hidden";
-        scoreContainer.style.visibility = "hidden";
-        answerContainer.style.visibility = "hidden";
-        //quizContainer.style.visibility = "hidden";
-    });
-    
+window.addEventListener("load", function (event) {
+	submitButton.style.visibility = "hidden";
+	nextButton.style.visibility = "hidden";
+	backButton.style.visibility = "hidden";
+	scoreContainer.style.visibility = "hidden";
+	answerContainer.style.visibility = "hidden";
+	//quizContainer.style.visibility = "hidden";
+});
 
-	//set up timer
-	var timeEl = document.querySelector("#time");
-	var startButton = document.querySelector("#start-button");
-	//variable to track remaining time
-	var secondsLeft = 75;
+//set up timer
+var timeEl = document.querySelector("#time");
+var startButton = document.querySelector("#start-button");
+//variable to track remaining time
+var secondsLeft = 75;
 
-	//function to start countdown
-	function countDown() {
-		timerInterval = setInterval(function () {
-			secondsLeft--;
+//function to start countdown
+function countDown() {
+	timerInterval = setInterval(function () {
+		secondsLeft--;
 
-			timeEl.textContent = "Time Left: " + secondsLeft;
+		timeEl.textContent = "Time Left: " + secondsLeft;
 
-			//if (secondsLeft ===0) {
-			//setup here the end of game and enter highscores etc
-			//}
-		}, 1000);
-	}
+		//if (secondsLeft ===0) {
+		//setup here the end of game and enter highscores etc
+		//}
+	}, 1000);
+}
 
+//function to start our quiz
+startButton.addEventListener("click", function (event) {
+	if (event.target.matches("button")) {
+		countDown();
+		//display questions and answers
+		instructionsContainer.textContent = questions[0].question;
+		aContainer.textContent = questions[0].answers.a;
+		bContainer.textContent = questions[0].answers.b;
+		cContainer.textContent = questions[0].answers.c;
+		dContainer.textContent = questions[0].answers.d;
 
-    
-
-   //function to start our quiz 
-   startButton.addEventListener("click", function (event) {
-    if (event.target.matches("button")) {
-        countDown();
-       //display questions and answers 
-       instructionsContainer.textContent = (questions[0].question);
-       aContainer.textContent = (questions[0].answers.a);
-       bContainer.textContent = (questions[0].answers.b);
-       cContainer.textContent = (questions[0].answers.c);
-       dContainer.textContent = (questions[0].answers.d);
-        //trigger quiz to display
-        //buildQuiz();
-
-     
-        //trigger buttons to display and other elements to hide
-        //instructionsContainer.style.visibility = "hidden";
-        startButton.style.visibility = "hidden";
-        submitButton.style.visibility = "visible";
-        nextButton.style.visibility = "visible";
-        backButton.style.visibility = "visible";
-        scoreContainer.style.visibility = "visible";
+		//trigger buttons to display and other elements to hide
+		//instructionsContainer.style.visibility = "hidden";
+		startButton.style.visibility = "hidden";
+		submitButton.style.visibility = "visible";
+		nextButton.style.visibility = "visible";
+		scoreContainer.style.visibility = "visible";
         answerContainer.style.visibility = "visible";
+        
+        aContainer.addEventListener("click", function (event) {
+            if(event.target.matches("button")) {
+                console.log(event);
+                scoreContainer.textContent = "Incorrect!";
+                //decrease time
+            }
 
+        })
+        bContainer.addEventListener("click", function (event) {
+            if(event.target.matches("button")) {
+                console.log(event);
+                scoreContainer.textContent = "Incorrect!";
+                //decrease time
+            }
 
-    }
+        })
+        cContainer.addEventListener("click", function (event) {
+            if(event.target.matches("button")) {
+                console.log(event);
+                scoreContainer.textContent = "Correct!";
+                //decrease time
+            }
+
+        })
+        dContainer.addEventListener("click", function (event) {
+            if(event.target.matches("button")) {
+                console.log(event);
+                scoreContainer.textContent = "Incorrect!";
+                //decrease time
+            }
+
+        })
+
+		//trigger quiz to display
+		//buildQuiz();
+	}
 });
